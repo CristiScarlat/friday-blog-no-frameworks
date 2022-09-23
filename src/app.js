43603d3903232befc08c93b6556f9c897a1bff11
scarlat-trainer <78.cristi@gmail.com>
2022-09-23 14:02:13 +0300

    add localstorage persistens of server data, add some components

--- src/app.js
+++ src/app.js
@@ -1,15 +1,57 @@
-import { nav, button } from './components/header';
-import { getAllExercises } from './services/api';
+import './index.css';
+import { nav } from './components/header/header';
+import filters from './components/filters/filters';
+import card from './components/card/card';
+import { getAllExercises, getBodyParts, getTargetList, getEquipmentList  } from './services/api';
+
+
+let filtersData = [];
+let exercises = [];
 
 const root = document.getElementById('root');
-root.append(nav);
+root.append(nav)
+
+
+
+const buildFilters = () => {
+    const filterContainer = filters(filtersData);
+    root.appendChild(filterContainer);
+}
+
+const buildExercisesList = () => {
+    const cardComponent = card(exercises[89])
+    root.appendChild(cardComponent);
+}
 
 window.onload = async () => {
-    try{
-        const res = await getAllExercises();
-        console.log(res)
-    } catch(error){
-        console.log(error)
+    const storedData = localStorage.getItem('filtersData');
+    if(storedData)filtersData = JSON.parse(storedData);
+    if(filtersData.length === 0){
+        try {
+            const res = await Promise.allSettled([getBodyParts(), getTargetList(), getEquipmentList()]);
+            res.forEach(data => {
+                if(data.status === 'fulfilled')filtersData.push(data.value.data);
+            })
+            if(filtersData.length === 3)localStorage.setItem('filtersData', JSON.stringify(filtersData));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    buildFilters()
+
+    const storedExercise = localStorage.getItem('allExercises');
+    if(storedExercise)exercises = JSON.parse(storedExercise);
+    if(exercises.length === 0){
+        try {
+            const res = await getAllExercises();
+            if(res.status === 200){
+                exercises = res.data;
+                localStorage.setItem('allExercises', JSON.stringify(res.data));
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
 
+    buildExercisesList();
 }
